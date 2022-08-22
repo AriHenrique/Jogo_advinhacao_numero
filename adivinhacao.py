99ddb62d1f3d3bc727b98d6cf56cf86d5600670f
Ari Henrique <arihenriqueuna@gmail.com>
2022-08-22 12:59:00 -0300

    update random jogo de advinhacao

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,30 +1,41 @@
-print("Bem vindo ao jogo de Adivinhação! Tente acertar o número secreto")
-secreto = int(input("Primeiro peça a outra pessoa digitar um número qualquer entre 1 e 100): "))
-while secreto < 1 or secreto > 100:
-    secreto = int(input("Você digitou um número menor que 1 ou maior que 100, "
-                        "digite NOVAMENTE um número qualquer entre 1 e 100): "))
+import random
 
-loop = int(input("Agora digite quantas tentativas quer ter (max 100): "))
-while loop > 100 or loop < 1:
-    loop = int(input("Você digitou um número de tentativas menor que 1 ou "
-                     "maior que 100, Digite um número de tentativas entre 1 e 100: "))
+continuar = 1
+while continuar == 1:
+    print("Bem vindo ao jogo de Adivinhação! Tente acertar o número secreto")
+    option = int(input('Antes de começar gostaria que o jogo definisse o número secreto'
+                       ' ou uma pessoa? digite 1 para Jogo ou 0 para Pessoa: '))
+    if option == 0:
+        secreto = int(input("Primeiro peça a outra pessoa digitar um número qualquer entre 1 e 100): "))
+        while secreto < 1 or secreto > 100:
+            secreto = int(input("Você digitou um número menor que 1 ou maior que 100, "
+                                "digite NOVAMENTE um número qualquer entre 1 e 100): "))
 
-for i in range(1, loop + 1):
-    chute = int(input("Digite o seu número : "))
-    print("Você digitou ", chute)
-
-    acertou = chute == secreto
-    maior = chute > secreto
-    menor = chute < secreto
-
-    if acertou:
-        print("ACERTOU!!!!")
-        break
+        loop = int(input("Agora digite quantas tentativas quer ter (max 100): "))
+        while loop > 100 or loop < 1:
+            loop = int(input("Você digitou um número de tentativas menor que 1 ou "
+                             "maior que 100, Digite um número de tentativas entre 1 e 100: "))
     else:
-        if maior:
-            print("ERROU!!! O CHUTE FOI MAIOR QUE O NÚMERO SECRETO")
-            print("VOCÊ TEM ", loop - i, " TENTATIVA(S)")
-        elif menor:
-            print("ERROU!!! O CHUTE FOI MENOR QUE O NÚMERO SECRETO")
-            print("VOCÊ TEM ", loop - i, " TENTATIVA(S)")
-print("FIM DE JOGO")
+        secreto = int(random.random() * 100)
+        loop = int(random.random() * 100)
+
+    for i in range(1, loop + 1):
+        chute = int(input("Digite o seu número : "))
+        print("Você digitou ", chute)
+
+        acertou = chute == secreto
+        maior = chute > secreto
+        menor = chute < secreto
+
+        if acertou:
+            print("ACERTOU!!!!")
+            break
+        else:
+            if maior:
+                print("ERROU!!! O CHUTE FOI MAIOR QUE O NÚMERO SECRETO")
+                print("VOCÊ TEM ", loop - i, " TENTATIVA(S)")
+            elif menor:
+                print("ERROU!!! O CHUTE FOI MENOR QUE O NÚMERO SECRETO")
+                print("VOCÊ TEM ", loop - i, " TENTATIVA(S)")
+    print("FIM DE JOGO O NÚMERO SECRETO ERA ", secreto)
+    continuar = int(input("Deseja jogar novamente? 1 pra sim e 0 pra não: "))
